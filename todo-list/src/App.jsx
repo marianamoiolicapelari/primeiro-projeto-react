25e2ff83a1a4d8f172620d2b8324b7c5028b43a1
Mariana M.C <marianamoioli@gmail.com>
2022-09-13 17:58:00 -0300

    functionality changes

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([{ id: uuid(), task: "Nada" }])
+
+
+  function inputMudou(event) {
+    console.log(event.target.value)
+    setList([{ id: uuid(), task: event.target.value }])
+
+    console.log(list)
+  }
+
+  function cliqueNoBotao() {
+    console.log("Cliquei no botão!")
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input onChange={inputMudou} placeholder="Digite o que tenha para fazer..." />
+      <button onClick={cliqueNoBotao}>Adicionar</button>
+
+      <ul>
+        {
+          list.map(item => (
+            <li key={item.id}>{item.task}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
