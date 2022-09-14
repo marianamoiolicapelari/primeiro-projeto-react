271f751f31aa8ea4d38ae4eb6da34cc6f1c0c769
Mariana M.C <marianamoioli@gmail.com>
2022-09-14 17:32:41 -0300

    modified styles.js

--- todo-list/src/styles.js
+++ todo-list/src/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    background: #383838;
+    background: #3838;
     width: 100vw;
     height: 100vh;
     display: flex;
